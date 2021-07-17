40dc1fe191034245e5bf53d636b617d472ceae9c
mitty1293 <k.fmichi@gmai.com>
2021-07-18 02:33:47 +0900

    fix

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM python:3.9.5-slim-buster
 RUN apt-get update && \
     apt-get install -y --no-install-recommends sqlite3 && \
     rm -rf /var/lib/apt/lists/*
-RUN echo "CREATE TABLE user(id, pass, salt);" | /usr/bin/sqlite3 /auth/data/user.db
+RUN mkdir -p /auth/data
+RUN echo "CREATE TABLE user (id, pass, salt);" | /usr/bin/sqlite3 /auth/data/user.db
 CMD [ "python", "/auth/src/main.py" ]
--- src/dbaccessor.py
+++ src/dbaccessor.py
@@ -8,11 +8,11 @@ class DbAccessor:
         self.cur = self.conn.cursor()
 
     def fetch_from_db(self, tbl_name, target_column, key) -> List:
-        self.cur.execute("SELECT * FROM %s WHERE %s = %s", tbl_name, target_column, key)
+        self.cur.execute("SELECT * FROM %s WHERE %s = %s;", (tbl_name, target_column, key))
         results = self.cur.fetchall()
         return results
 
     def store_to_db(self, tbl_name, stored_id, stored_pass, stored_salt):
-        self.cur.execute("INSERT INTO %s (id, pass, salt) VALUES (%s, %s, %s)", tbl_name, stored_id, stored_pass, stored_salt)
+        self.cur.execute("INSERT INTO user (id, pass, salt) VALUES (%s, %s, %s);", (stored_id, stored_pass, stored_salt))
         self.cur.commit()
         return 0
