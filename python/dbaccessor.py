5f5f37ac5549df113bf41aa5af22343dd23437e4
mitty1293 <k.fmichi@gmai.com>
2021-07-17 19:48:11 +0900

    コンストラクタ実装

--- python/dbaccessor.py
+++ python/dbaccessor.py
@@ -3,4 +3,8 @@ import sqlite3
 
 class DbAccessor():
     def __init__(self) -> None:
-        conn = sqlite3.connect()
+        self.conn = sqlite3.connect("/data/user.db")
+        self.cur = self.conn.cursor()
+
+    def aaa():
+        pass
