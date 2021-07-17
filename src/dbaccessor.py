ba48ebd9900d088f54fb9c7c2f1e3ae8ae00e914
mitty1293 <k.fmichi@gmai.com>
2021-07-18 01:29:41 +0900

    DBAccessor完成

--- src/dbaccessor.py
+++ src/dbaccessor.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 import sqlite3
+from typing import List
 
 class DbAccessor:
     def __init__(self, db_file) -> None:
         self.conn = sqlite3.connect(db_file)
         self.cur = self.conn.cursor()
 
-    def aaa():
-        pass
+    def fetch_from_db(self, tbl_name, target_column, key) -> List:
+        self.cur.execute("SELECT * FROM %s WHERE %s = %s", tbl_name, target_column, key)
+        results = self.cur.fetchall()
+        return results
+
+    def store_to_db(self, tbl_name, stored_id, stored_pass, stored_salt):
+        self.cur.execute("INSERT INTO %s (id, pass, salt) VALUES (%s, %s, %s)", tbl_name, stored_id, stored_pass, stored_salt)
+        self.cur.commit()
+        return 0
