d143945cc4decb271a06bf629c520166dffa9d22
mitty1293 <k.fmichi@gmai.com>
2021-07-17 13:24:05 +0900

    userクラスの定義

---
+++ src/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+class User():
+    def __init__(self) -> None:
+        self.id = ""
+        self.pass = ""
+    
+    def encrypt(self, plaintext):
+        pass
+
+    def decrypt(self, ciphertext):
+        pass
