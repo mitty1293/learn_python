b97d20079c5a1a5c7e5500e3d9d0a8f3b0b80298
mitty1293 <k.fmichi@gmai.com>
2021-07-19 02:51:59 +0900

    Rowオブジェクト化

--- src/dbaccessor.py
+++ src/dbaccessor.py
@@ -10,31 +10,27 @@ class DbAccessor:
             db_file (str): SQLite database file path
         """
         self.conn = sqlite3.connect(db_file)
+        self.conn.row_factory = sqlite3.Row
         self.cur = self.conn.cursor()
 
-    def fetch_from_db(self, user_id: str) -> List[Tuple[str, str, str]]:
+    def fetch_from_db(self, user_id: str) -> sqlite3.Row:
         """Select a row of data from 'user' table
 
         Args:
             user_id (str): 取得したいレコードのuser_id
 
         Returns:
-            List[Tuple[str, str, str]]: user_idが存在する場合は紐づくレコード。存在しない場合は空リスト。
+            sqlite3.Row: user_idが存在する場合は紐づくレコードをRowオブジェクト（like 辞書）で返す。
+                         存在しない場合はNoneが返る。
         
         Note:
             本メソッドはレコード取得までが範囲であるため、呼び出し元で存在判定することが望ましい。
 
-            pythonにおいて空リスト [] は False と判定される。
-            その他のリストは全て True 判定となる。
-
-            呼び出し元では以下のように判定すれば良い。
-            if results:
-                return results
-            else:
-                return "list is empty"
+            user_idが存在する場合、返り値であるRowオブジェクトは空ではないのでTrue判定される。
+            user_idが存在しない場合、返り値であるNoneはpythonではFalse判定される。
         """
         self.cur.execute('SELECT * FROM user WHERE user_id = ?', (user_id,))
-        return self.cur.fetchall()
+        return self.cur.fetchone()
 
     def store_to_db(self, user_id: str, user_pass: str, salt: str) -> bool:
         """Insert a row of data to 'user' table
