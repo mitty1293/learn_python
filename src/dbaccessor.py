15160b0758b2b082eb54800b470035a4fdf17699
mitty1293 <k.fmichi@gmai.com>
2021-07-18 20:21:00 +0900

    カラム名を明確にした

--- src/dbaccessor.py
+++ src/dbaccessor.py
@@ -8,11 +8,11 @@ class DbAccessor:
         self.cur = self.conn.cursor()
 
     def fetch_from_db(self, key: str) -> List[Tuple[str, str, str]]:
-        self.cur.execute('SELECT * FROM user WHERE id = ?', (key,))
+        self.cur.execute('SELECT * FROM user WHERE user_id = ?', (key,))
         results = self.cur.fetchall()
         return results
 
     def store_to_db(self, stored_id: str, stored_pass: str, stored_salt: str) -> int:
-        self.cur.execute('INSERT INTO user (id, pass, salt) VALUES (?, ?, ?)', (stored_id, stored_pass, stored_salt))
+        self.cur.execute('INSERT INTO user (user_id, user_pass, salt) VALUES (?, ?, ?)', (stored_id, stored_pass, stored_salt))
         self.conn.commit()
         return 0
