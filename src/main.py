6fdf484c65c5c78c52252132f4efcc21fd5c5a59
mitty1293 <k.fmichi@gmai.com>
2021-07-20 02:18:27 +0900

    dbaccessor.DbAccessorのグローバル化

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import user, secrets, string
 import dbaccessor, user
 
 chars: str = string.ascii_uppercase + string.ascii_lowercase + string.ascii_letters + string.digits + '_' + '-' + '!'
+cur = dbaccessor.DbAccessor("/auth/data/user.db")
 
 def main():
     sign: int = int(input("Sign up:0, Sign in:1 -> "))
@@ -28,7 +29,7 @@ def sign_up():
     new_user.create_hash(new_user.user_pass, salt)
     print(salt)
 
-    cur = dbaccessor.DbAccessor("/auth/data/user.db")
+    # cur = dbaccessor.DbAccessor("/auth/data/user.db")
     is_succeeded: bool
     if cur.store_to_db(new_user.user_id, new_user.hashed_pass, salt):
         return(is_succeeded := True)
