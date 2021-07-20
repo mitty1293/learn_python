df6638c3ff787f002848edc57e26735d518c4f59
mitty1293 <k.fmichi@gmai.com>
2021-07-20 16:07:06 +0900

    signin実装

--- src/account_manager.py
+++ src/account_manager.py
@@ -6,7 +6,7 @@ class AccountManager:
     chars: str = string.ascii_uppercase + string.ascii_lowercase + string.ascii_letters + string.digits + '_' + '-' + '!'
     cur = dbaccessor.DbAccessor("/auth/data/user.db")
 
-    def signup(self, user_id: str, user_pass: str):
+    def signup(self, user_id: str, user_pass: str) -> bool:
         new_user = user.User()
         new_user.user_id = user_id
         new_user.user_pass = user_pass
@@ -17,4 +17,16 @@ class AccountManager:
         is_succeeded: bool
         if self.cur.store_to_db(new_user.user_id, new_user.hashed_pass, salt):
             return(is_succeeded := True)
+        return (is_succeeded := False)
+
+    def signin(self, user_id: str, user_pass: str) -> bool:
+        existing_user = user.User()
+        existing_user.user_id = user_id
+        existing_user.user_pass = user_pass
+
+        is_succeeded: bool
+        if (db_data := self.cur.fetch_from_db(existing_user.user_id)):
+            existing_user.create_hash(existing_user.user_pass, db_data["salt"])
+            if db_data["user_pass"] == existing_user.hashed_pass:
+                return (is_succeeded := True)
         return (is_succeeded := False)
