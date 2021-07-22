6a7950efebc6d1a7442e2585de9af6a9ea463e04
mitty1293 <k.fmichi@gmai.com>
2021-07-22 21:51:57 +0900

    ラップしてみた

--- src/input_value.py
+++ src/input_value.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 import hashlib
 
+class UserInfo():
+    def __init__(self) -> None:
+        self.user_id: str = "user_id"
+        self.user_pass: str = "user_pass"
+
+class HashedInfo():
+    def __init__(self) -> None:
+        self.hashed_pass: str = "hashed_pass"
+
 class InputValue():
     def __init__(self) -> None:
-        self.user_id:str = ""
-        self.user_pass:str = ""
-        self.hashed_pass:str = ""
+        self.user_info = UserInfo()
+        self.hashed_info = HashedInfo()
+        # self.user_id:str = ""
+        # self.user_pass:str = ""
+        # self.hashed_pass:str = ""
+
 
     def create_hash(self, salt: str) -> None:
         """[summary]
