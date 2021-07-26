566528fb2d04bf9efd3d2b084f74682bd1d96855
mitty1293 <k.fmichi@gmai.com>
2021-07-26 22:14:48 +0900

    fix

--- src/input_value.py
+++ src/input_value.py
@@ -6,7 +6,11 @@ class InputValue():
         self._user_id = UserId(user_id)
         self._user_pass = UserPass(user_pass)
 
-    def 
+    def user_id(self):
+        return self._user_id.user_id()
+
+    def user_pass(self):
+        return self._user_pass.user_pass()
 
     def create_hash(self, salt):
         return self._user_pass.create_hash(salt)
@@ -15,9 +19,15 @@ class UserId():
     def __init__(self, user_id) -> None:
         self.user_id: str = user_id
 
+    def return_value(self):
+        return self.user_id
+
 class UserPass():
     def __init__(self, user_pass) -> None:
         self.user_pass: str = user_pass
+
+    def return_value(self):
+        return self.user_pass
 
     def create_hash(self, salt):
         return hashlib.sha256((self.user_pass + salt).encode()).hexdigest()
