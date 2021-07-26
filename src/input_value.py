38a810d7f94d5286e5a64c5587a2ddeed5a77e29
mitty1293 <k.fmichi@gmai.com>
2021-07-26 17:28:18 +0900

    fix

--- src/input_value.py
+++ src/input_value.py
@@ -14,19 +14,8 @@ class UserPass():
 
 class InputValue():
     def __init__(self, user_id, user_pass) -> None:
-        self.user_id = UserId(user_id)
-        self.user_pass = UserPass(user_pass)
+        self._user_id = UserId(user_id)
+        self._user_pass = UserPass(user_pass)
 
-    def create_hash(self, salt: str) -> None:
-        """[summary]
-
-        Args:
-            salt (str): [description]
-
-        Note:
-            str.encode(): strをutf-8でエンコードしたbytesオブジェクトを返す
-            hashlib.sha256(bytes): sha256で暗号化されたhashオブジェクトを返す
-            hash.hexdigest: hashオブジェクトの16進形式文字列を返す
-        """
-        self.hashed_pass = hashlib.sha256((self.user_pass + salt).encode()).hexdigest()
-        # hashlib.pbkdf2_hmac(hash_name, password, salt, iterations)
+    def create_hash(self, salt):
+        return self._user_pass.create_hash(salt)
--- src/test.py
+++ src/test.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 import hashlib
 
-class StringValue:
+class UserInfo:
     def __init__(self) -> None:
-        self._str = ""
+        self._user_info = {}
 
-    def assign(self, string_):
-        self._str = string_
+    def refer_to(self, value):
+        return self._user_info[value]
+
+    def add(self, user_id, user_pass):
+        self._user_info["id"] = user_id
+        self._user_info["pass"] = user_pass
 
     def create_hash(self, salt):
-        return hashlib.sha256((self._str + salt).encode()).hexdigest()
+        return hashlib.sha256((self._user_info["pass"] + salt).encode()).hexdigest()
