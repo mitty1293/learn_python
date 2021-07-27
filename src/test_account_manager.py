6dd0a4435c68a2775fb5eef44da829bac353709e
mitty1293 <k.fmichi@gmai.com>
2021-07-27 18:22:37 +0900

    testcase作成

--- src/test_account_manager.py
+++ src/test_account_manager.py
@@ -3,13 +3,14 @@ from account_manager import AccountManager
 
 class TestAccount(unittest.TestCase):
     am = AccountManager("/auth/data/user.db")
-    def test_signup_success(self):
+    test_user_id = "test" + ""
+    def test1_signup_success(self):
         self.assertTrue(self.am.signup("test1","truepass"))
-    def test_signup_fail(self):
+    def test2_signup_fail(self):
         self.assertFalse(self.am.signup("test1","truepass"))
-    def test_signin_success(self):
+    def test3_signin_success(self):
         self.assertTrue(self.am.signin("test1","truepass"))
-    def test_signin_fail(self):
+    def test4_signin_fail(self):
         self.assertFalse(self.am.signin("test1","wrongpass"))
 
 if __name__ == '__main__':
