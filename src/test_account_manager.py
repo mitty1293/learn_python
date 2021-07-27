43298d0afc6335685579f648b6a95002242d3f54
mitty1293 <k.fmichi@gmai.com>
2021-07-27 17:39:44 +0900

    unittest作成

---
+++ src/test_account_manager.py
@@ -0,0 +1,11 @@
+import unittest
+
+class Test(unittest.TestCase):
+    def test_signup_success(self):
+        pass
+    def test_signup_fail(self):
+        pass
+    def test_signin_success(self):
+        pass
+    def test_signin_fail(self):
+        pass
