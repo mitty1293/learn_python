e07b2a339fe9811cddcc30ead16234dcef37af19
mitty1293 <k.fmichi@gmai.com>
2021-07-27 15:57:16 +0900

    docstring追記

--- src/input_value.py
+++ src/input_value.py
@@ -2,30 +2,63 @@
 import hashlib
 
 class InputValue():
-    def __init__(self, user_id, user_pass) -> None:
+    def __init__(self, user_id: str, user_pass: str) -> None:
+        """Constructor
+
+        Args:
+            user_id (str): User ID of the account
+            user_pass (str): Password of the account
+        """
         self._user_id = String(user_id)
         self._user_pass = String(user_pass)
 
-    def user_id(self):
+    def user_id(self) -> str:
+        """Return a value of self._user_id
+
+        Returns:
+            str: A value of self._user_id
+        """
         return self._user_id.return_value()
 
-    def hashed_pass(self, salt):
+    def hashed_pass(self, salt: str) -> str:
+        """Hashing a value of self._user_pass
+
+        Args:
+            salt (str): A string to randomizes the output of the password hash
+
+        Returns:
+            str: Hash string of self._user_pass
+        """
         return self._user_pass.hashing_str(salt)
 
 class String():
-    def __init__(self, value) -> None:
+    def __init__(self, value: str) -> None:
+        """Constructor
+
+        Args:
+            value (str): Value
+        """
         self.value: str = value
 
-    def return_value(self):
+    def return_value(self) -> str:
+        """Return a value of class "String"
+
+        Returns:
+            str: A value of class "String"
+        """
         return self.value
 
-    def hashing_str(self, salt):
-        """[summary]
+    def hashing_str(self, salt: str) -> str:
+        """Hashing a value of class "String"
         Args:
-            salt (str): [description]
+            salt (str): A string to randomizes the output of the password hash
+
+        Returns:
+            str: Hash string of class "String"
+
         Note:
-            str.encode(): strをutf-8でエンコードしたbytesオブジェクトを返す
-            hashlib.sha256(bytes): sha256で暗号化されたhashオブジェクトを返す
-            hash.hexdigest: hashオブジェクトの16進形式文字列を返す
+            str.encode(): Return a bytes object with str encoded in utf-8
+            hashlib.sha256(bytes_object): Return a SHA-256 hash object
+            hash_object.hexdigest(): Return as a string object containing only hexadecimal digits
         """
         return hashlib.sha256((self.value + salt).encode()).hexdigest()
