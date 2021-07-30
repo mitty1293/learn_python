1f82a152728a9535fab12981c7ff3516dfabcf50
mitty1293 <k.fmichi@gmai.com>
2021-07-31 01:17:36 +0900

    testcase修正

--- src/test.py
+++ src/test.py
@@ -1,21 +1,23 @@
 import unittest
 import auth
 
-class TestAccount(unittest.TestCase):
+class TestAuth(unittest.TestCase):
     am = auth.AccountManager("/auth/data/user.db")
-    test_user_id = "test" + ""
+    test_user_id = "test_user_id"
+    test_truepass = "test_truepass"
+    test_wrongpass = "test_wrongpass"
     def test1_signup_success(self):
-        self.assertTrue(self.am.signup("test1","truepass"))
+        self.assertTrue(self.am.signup(test_user_id,test_truepass))
     def test2_signup_fail(self):
-        self.assertFalse(self.am.signup("test1","truepass"))
+        self.assertFalse(self.am.signup(test_user_id,test_truepass))
     def test3_signin_success(self):
-        self.assertTrue(self.am.signin("test1","truepass"))
+        self.assertTrue(self.am.signin(test_user_id,test_truepass))
     def test4_signin_fail(self):
-        self.assertFalse(self.am.signin("test1","wrongpass"))
+        self.assertFalse(self.am.signin(test_user_id,test_wrongpass))
     def test5_delete_success(self):
-        self.assertTrue(self.am.delete_user("test1","truepass"))
+        self.assertTrue(self.am.delete_user(test_user_id,test_truepass))
     def test6_delete_fail(self):
-        self.assertFalse(self.am.delete_user("test1","truepass"))
+        self.assertFalse(self.am.delete_user(test_user_id,test_truepass))
 
 if __name__ == '__main__':
     unittest.main()
