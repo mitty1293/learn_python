ef8ac39621f2e5676f2db0c0acb97f9fa300a021
mitty1293 <k.fmichi@gmail.com>
2021-11-07 16:53:41 +0900

    平均算出処理追加

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import csv
 
-def import_csv(csvpath: str):
+def set_score_dict(csvpath: str):
     score_dict: dict = {}
     with open(csvpath, 'r', encoding='utf-8') as f:
         csv_reader = csv.reader(f, delimiter=',')
-        for line in csv_reader:
-            create_timestamp, player_id, score = line
+        header = next(csv_reader)
+        for create_timestamp, player_id, score in csv_reader:
             if player_id not in score_dict:
-                sum_score = score
+                sum_score = int(score)
                 count = 1
             else:
-                sum_score = score_dict[player_id][0] + score
-                count = score_dict[player_id][1] + 1
-            score_dict[player_id] = [sum_score, count]
+                sum_score = score_dict[player_id]['sum_score'] + int(score)
+                count = score_dict[player_id]['count'] + 1
+            score_dict[player_id] = {'sum_score': sum_score, 'count': count}
     return score_dict
-# 次はplayer_id毎の平均点を出す⇒ソートしてランキングにする
+
+def set_mean_dict(score_dict: dict):
+    mean_dict: dict = {}
+    for player_id in score_dict.keys():
+        mean_score = score_dict[player_id]['sum_score'] / score_dict[player_id]['count']
+        mean_dict[player_id] = mean_score
+    return mean_dict
 
