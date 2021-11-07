ed4677df8ad2ca58a9a01e6fa8dd32d27cb7e020
mitty1293 <k.fmichi@gmail.com>
2021-11-07 17:39:00 +0900

    完成

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import csv
 
-def set_score_dict(csvpath: str):
+def main():
+    csvpath: str = input("CSV Path ->")
+    score_dict: dict = set_score_dict(csvpath)
+    mean_dict: dict = set_mean_dict(score_dict)
+    print_ranking(mean_dict)
+
+def set_score_dict(csvpath: str) -> dict:
     score_dict: dict = {}
     with open(csvpath, 'r', encoding='utf-8') as f:
         csv_reader = csv.reader(f, delimiter=',')
@@ -15,10 +21,20 @@ def set_score_dict(csvpath: str):
             score_dict[player_id] = {'sum_score': sum_score, 'count': count}
     return score_dict
 
-def set_mean_dict(score_dict: dict):
+def set_mean_dict(score_dict: dict) -> dict:
     mean_dict: dict = {}
     for player_id in score_dict.keys():
         mean_score = score_dict[player_id]['sum_score'] / score_dict[player_id]['count']
         mean_dict[player_id] = mean_score
     return mean_dict
 
+def print_ranking(mean_dict: dict) -> None:
+    rank_list: list = sorted(mean_dict.items(), key=lambda i: i[1], reverse=True)
+    print("rank, player_id, mean_score")
+    rank: int = 1
+    for player_id, mean_score in rank_list:
+        print(f'{rank}, {player_id}, {mean_score}')
+        rank += 1
+
+if __name__ == '__main__':
+    main()
