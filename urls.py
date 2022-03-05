8b7b5a772ebd8b0df268241f8120c907ae75c79b
mitty1293 <k.fmichi@gmail.com>
2022-03-05 14:39:48 +0900

    chapter20 step1完了

--- urls.py
+++ urls.py
@@ -6,5 +6,6 @@ url_patterns = [
     URLPattern("/now", views.now),
     URLPattern("/show_request", views.show_request),
     URLPattern("/parameters", views.parameters),
-    URLPattern("/user/<user_id>/profile", views.user_profile)
+    URLPattern("/user/<user_id>/profile", views.user_profile),
+    URLPattern("/set_cookie", views.set_cookie)
 ]
