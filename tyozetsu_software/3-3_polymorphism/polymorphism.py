26e07cecc0ba0081ce1d41df5df8f3d8afa81bee
mitty1293 <46413939+mitty1293@users.noreply.github.com>
2023-07-22 23:36:44 +0900

    Update polymorphism.py

--- tyozetsu_software/3-3_polymorphism/polymorphism.py
+++ tyozetsu_software/3-3_polymorphism/polymorphism.py
@@ -23,6 +23,14 @@ customer.touch(Dog())
 customer.touch(Cat())
 
 """
+どんなPetであろうとtouchすればreactionが起きる
+Petオブジェクト毎に異なる機能や動作を示すことができる
+PetとCustomerはこの二者だけで完結しており, 具体的なDogやCatには興味を持たない
+
+実行される処理の実体がコールされたメソッドではなく, メソッドを持つオブジェクトによって決定される性質.
+またこの性質を使って, 同一のメソッドを使って, オブジェクトごとに異なった処理を行わせること.
+-> 多態性
+
 $ python polymorphism.py 
 Bow-Wow!
 Meow-Meow!
